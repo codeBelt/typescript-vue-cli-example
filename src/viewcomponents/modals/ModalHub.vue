712aa66d82a59fc965155b7dd8d6e0325fae8807
Robert S <rsavian@nerdery.com>
2019-01-05 08:22:30 -0600

    modal hub

---
+++ src/viewcomponents/modals/ModalHub.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>ModalHub</div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component
+export default class ModalHub extends Vue {}
+</script>
+
+<style scoped lang="scss"></style>
